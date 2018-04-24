8d94d8ef0617900d6b901a8ee1d6dd22f91835ea
Ben Mason <ben.mason8@gmail.com>
2018-04-23 22:09:44 -0700

    Content provider partially set up.

--- app/src/main/java/com/example/android/popularmovies/data/MovieProvider.java
+++ app/src/main/java/com/example/android/popularmovies/data/MovieProvider.java
@@ -1,9 +1,11 @@
 package com.example.android.popularmovies.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -34,29 +36,85 @@ public class MovieProvider extends ContentProvider {
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+        Cursor cursor;
+
+        switch (sUriMatcher.match(uri)) {
+            case CODE_MOVIE:
+                cursor = mMovieDbHelper.getReadableDatabase().query(
+                        MovieContract.MovieDbEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
     }
 
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+        final SQLiteDatabase sqLiteDatabase = mMovieDbHelper.getWritableDatabase();
+
+        switch (sUriMatcher.match(uri)) {
+            case CODE_MOVIE:
+                sqLiteDatabase.beginTransaction();
+                try {
+                    long rowId = sqLiteDatabase.insert(MovieContract.MovieDbEntry.TABLE_NAME, null,
+                            values);
+                    if (rowId > 0) {
+                        getContext().getContentResolver().notifyChange(uri, null);
+                        sqLiteDatabase.setTransactionSuccessful();
+                        return ContentUris.withAppendedId(MovieContract.MovieDbEntry.CONTENT_URI,
+                                rowId);
+                    }
+                } finally {
+                    sqLiteDatabase.endTransaction();
+                }
+                break;
+        }
+        throw new UnsupportedOperationException("Unknown uri: " + uri);
     }
 
     @Override
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+        int numRowsDeleted;
+
+        if (selection == null) selection = "1";
+
+        switch (sUriMatcher.match(uri)) {
+            case CODE_MOVIE:
+                numRowsDeleted = mMovieDbHelper.getWritableDatabase().delete(
+                        MovieContract.MovieDbEntry.TABLE_NAME,
+                        selection,
+                        selectionArgs);
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Unknown Uri: " + uri);
+        }
+
+        if (numRowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return numRowsDeleted;
     }
 
     // Not used in this implementation
-    @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-        return null;
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     @Override
-    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+    public int update(@NonNull Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
